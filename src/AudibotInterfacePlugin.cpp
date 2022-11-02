05182b182424cd1c5288cb5e2f46c4fb8c37aa09
Micho Radovnikovich <mradovnikovich@dataspeedinc.com>
2022-11-01 21:28:17 -0400

    Rebase from 'release/melodic/audibot_gazebo'

--- src/AudibotInterfacePlugin.cpp
+++ src/AudibotInterfacePlugin.cpp
@@ -13,14 +13,13 @@ AudibotInterfacePlugin::AudibotInterfacePlugin() {
 
 void AudibotInterfacePlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf) {
   // Gazebo initialization
-  steer_fl_joint_ = model->GetJoint("steer_fl");
-  steer_fr_joint_ = model->GetJoint("steer_fr");
-  wheel_rl_joint_ = model->GetJoint("wheel_rl");
-  wheel_rr_joint_ = model->GetJoint("wheel_rr");
-  wheel_fl_joint_ = model->GetJoint("wheel_fl");
-  wheel_fr_joint_ = model->GetJoint("wheel_fr");
+  steer_fl_joint_ = model->GetJoint("steer_fl_joint");
+  steer_fr_joint_ = model->GetJoint("steer_fr_joint");
+  wheel_rl_joint_ = model->GetJoint("wheel_rl_joint");
+  wheel_rr_joint_ = model->GetJoint("wheel_rr_joint");
+  wheel_fl_joint_ = model->GetJoint("wheel_fl_joint");
+  wheel_fr_joint_ = model->GetJoint("wheel_fr_joint");
   footprint_link_ = model->GetLink("base_footprint");
-
   // Load SDF parameters
   if (sdf->HasElement("pubTf")) {
     sdf->GetElement("pubTf")->GetValue()->Get(pub_tf_);
@@ -37,12 +36,6 @@ void AudibotInterfacePlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf) 
     }
   } else {
     robot_name_ = std::string("");
-  }
-
-  if (sdf->HasElement("pubTf")) {
-    sdf->GetElement("pubTf")->GetValue()->Get(pub_tf_);
-  } else {
-    pub_tf_ = false;
   }
 
   if (sdf->HasElement("tfFreq")) {
@@ -66,10 +59,18 @@ void AudibotInterfacePlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf) 
 
   pub_twist_ = n_->advertise<geometry_msgs::TwistStamped> ("twist", 1);
   pub_gear_state_ = n_->advertise<std_msgs::UInt8> ("gear_state", 1);
+  pub_odom_ = n_->advertise<nav_msgs::Odometry>("odom", 1);
+  pub_steering_ = n_->advertise<std_msgs::Float64>("steering_state", 1);
   feedback_timer_ = n_->createTimer(ros::Duration(0.02), &AudibotInterfacePlugin::feedbackTimerCallback, this);
 
   if (pub_tf_) {
     tf_timer_ = n_->createTimer(ros::Duration(1.0 / tf_freq_), &AudibotInterfacePlugin::tfTimerCallback, this);
+  }
+
+  if (robot_name_.empty()) {
+    frame_id_ = footprint_link_->GetName();
+  } else {
+    frame_id_ = robot_name_ + "/" + footprint_link_->GetName();
   }
 }
 
@@ -242,7 +243,7 @@ void AudibotInterfacePlugin::recvGearCmd(const std_msgs::UInt8ConstPtr& msg) {
 
 void AudibotInterfacePlugin::feedbackTimerCallback(const ros::TimerEvent& event) {
   geometry_msgs::TwistStamped twist_msg;
-  twist_msg.header.frame_id = tf::resolve(robot_name_, footprint_link_->GetName());
+  twist_msg.header.frame_id = frame_id_;
   twist_msg.header.stamp = event.current_real;
   twist_msg.twist = twist_;
   pub_twist_.publish(twist_msg);
@@ -250,19 +251,52 @@ void AudibotInterfacePlugin::feedbackTimerCallback(const ros::TimerEvent& event)
   std_msgs::UInt8 gear_state_msg;
   gear_state_msg.data = gear_cmd_;
   pub_gear_state_.publish(gear_state_msg);
+
+  nav_msgs::Odometry odom_msg;
+  odom_msg.header.frame_id = frame_id_;
+  odom_msg.header.stamp = event.current_real;
+  odom_msg.twist.twist = twist_;
+  odom_msg.pose.pose.position.x = world_pose_.Pos().X();
+  odom_msg.pose.pose.position.y = world_pose_.Pos().Y();
+  odom_msg.pose.pose.position.z = world_pose_.Pos().Z();
+  odom_msg.pose.pose.orientation.x = world_pose_.Rot().X();
+  odom_msg.pose.pose.orientation.y = world_pose_.Rot().Y();
+  odom_msg.pose.pose.orientation.z = world_pose_.Rot().Z();
+  odom_msg.pose.pose.orientation.w = world_pose_.Rot().W();
+  pub_odom_.publish(odom_msg);
+
+  std_msgs::Float64 steering;
+  steering.data = AUDIBOT_STEERING_RATIO * current_steering_angle_;
+  pub_steering_.publish(steering);
 }
 
 void AudibotInterfacePlugin::tfTimerCallback(const ros::TimerEvent& event) {
-  tf::StampedTransform t;
-  t.frame_id_ = "world";
-  t.child_frame_id_ = tf::resolve(robot_name_, footprint_link_->GetName());
-  t.stamp_ = event.current_real;
+  // Don't publish TF if the same timestamp as last time
+  // to prevent TF_REPEATED_DATA warning
+  if ((event.current_real - event.last_real).toSec() < 1e-6) {
+    return;
+  }
+  
+  geometry_msgs::TransformStamped t;
+  t.header.frame_id = "world";
+  t.child_frame_id = frame_id_;
+  t.header.stamp = event.current_real;
 #if GAZEBO_MAJOR_VERSION >= 9
-  t.setOrigin(tf::Vector3(world_pose_.Pos().X(), world_pose_.Pos().Y(), world_pose_.Pos().Z()));
-  t.setRotation(tf::Quaternion(world_pose_.Rot().X(), world_pose_.Rot().Y(), world_pose_.Rot().Z(), world_pose_.Rot().W()));
+  t.transform.translation.x = world_pose_.Pos().X();
+  t.transform.translation.y = world_pose_.Pos().Y();
+  t.transform.translation.z = world_pose_.Pos().Z();
+  t.transform.rotation.w = world_pose_.Rot().W();
+  t.transform.rotation.x = world_pose_.Rot().X();
+  t.transform.rotation.y = world_pose_.Rot().Y();
+  t.transform.rotation.z = world_pose_.Rot().Z();
 #else
-  t.setOrigin(tf::Vector3(world_pose_.pos.x, world_pose_.pos.y, world_pose_.pos.z));
-  t.setRotation(tf::Quaternion(world_pose_.rot.x, world_pose_.rot.y, world_pose_.rot.z, world_pose_.rot.w));
+  t.transform.translation.x = world_pose_.pos.x;
+  t.transform.translation.y = world_pose_.pos.y;
+  t.transform.translation.z = world_pose_.pos.z;
+  t.transform.rotation.w = world_pose_.rot.w;
+  t.transform.rotation.x = world_pose_.rot.x;
+  t.transform.rotation.y = world_pose_.rot.y;
+  t.transform.rotation.z = world_pose_.rot.z;
 #endif
   br_.sendTransform(t);
 }
